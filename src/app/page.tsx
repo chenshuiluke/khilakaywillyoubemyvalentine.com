8cfdeccec688516aa3b31fafd6d7c46cc9a68994
lukecharta <Luke@getcharta.com>
2024-02-06 16:18:01 -0500

    Added text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,16 @@ import { useEffect, useRef, useState } from "react";
 import FlyingHeart from "./components/flying_heart";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {Array.from(Array(100).keys()).map((el) => {
-        return <FlyingHeart key={el} />;
-      })}
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="heading colored">Khilakay,</h1>
+      <h1 className="heading">
+        would you do me the honor of being my Valentine?
+      </h1>
+      <div>
+        {Array.from(Array(100).keys()).map((el) => {
+          return <FlyingHeart key={el} />;
+        })}
+      </div>
     </main>
   );
 }

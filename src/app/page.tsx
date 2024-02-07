a8e3772b8d25fb16407b7db278e1bb9660374e38
lukecharta <Luke@getcharta.com>
2024-02-07 13:11:13 -0500

    Added more pics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,11 @@ import LukeBald from "../../public/luke_bald.jpg";
 import TunUpFood from "../../public/tun_up_food.jpg";
 import Kiss from "../../public/kiss.jpg";
 import Thick from "../../public/thick.jpg";
+import Backshot from "../../public/backshot.jpg";
+import Collage from "../../public/collage.jpg";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-5 md:p-24">
       <h1 className="heading colored">Khilakay...</h1>
       <motion.div
         className="frame"
@@ -32,7 +34,7 @@ export default function Home() {
         <Image
           src={Painting}
           alt={"Painting of me and Khilakay"}
-          style={{ height: "90vh", width: "auto" }}
+          style={{ width: "auto" }}
         />
       </motion.div>
       <h1 className="heading">This was you before we met</h1>
@@ -48,7 +50,7 @@ export default function Home() {
         {/* <Image
           src={HumbleBeginnings}
           alt={"Painting of me and Khilakay"}
-          style={{ height: "90vh", width: "auto" }}
+          style={{ width: "auto" }}
         /> */}
       </motion.div>
       <h1 className="heading">This was me before we met</h1>
@@ -61,7 +63,7 @@ export default function Home() {
         <Image
           src={LukeBald}
           alt={"Painting of me and Khilakay"}
-          style={{ height: "90vh", width: "auto" }}
+          style={{ width: "auto" }}
         />
       </motion.div>
       <h1 className="heading">See how me did bald and stress out???</h1>
@@ -75,7 +77,20 @@ export default function Home() {
         <Image
           src={TunUpFood}
           alt={"Painting of me and Khilakay"}
-          style={{ height: "90vh", width: "auto" }}
+          style={{ width: "auto" }}
+        />
+      </motion.div>
+      <h1 className="heading">...and gave you some good good backshots...</h1>
+      <motion.div
+        className="frame"
+        initial={{ transform: `rotate(-10deg) translate(-100%, 0)` }}
+        whileInView={{ transform: `rotate(0deg) translate(0%, 0)` }}
+        transition={{ duration: 2 }}
+      >
+        <Image
+          src={Backshot}
+          alt={"Painting of me and Khilakay"}
+          style={{ width: "auto" }}
         />
       </motion.div>
       <h1 className="heading">...and got some of ur good loving...</h1>
@@ -88,7 +103,7 @@ export default function Home() {
         <Image
           src={Kiss}
           alt={"Painting of me and Khilakay"}
-          style={{ height: "90vh", width: "auto" }}
+          style={{ width: "auto" }}
         />
       </motion.div>
       <h1 className="heading">
@@ -103,14 +118,30 @@ export default function Home() {
         <Image
           src={Thick}
           alt={"Painting of me and Khilakay"}
-          style={{ height: "90vh", width: "auto" }}
+          style={{ width: "auto" }}
+        />
+      </motion.div>
+      <h1 className="heading">
+        I'm now the happiest I've ever been in my life and it's all because of
+      </h1>
+      <h1 className="heading colored">You</h1>
+      <motion.div
+        className="frame"
+        initial={{ transform: `rotate(-10deg) translate(-100%, 0)` }}
+        whileInView={{ transform: `rotate(0deg) translate(0%, 0)` }}
+        transition={{ duration: 2 }}
+      >
+        <Image
+          src={Collage}
+          alt={"Painting of me and Khilakay"}
+          style={{ width: "auto" }}
         />
       </motion.div>
       {/* <h1 className="heading">
         would you do me the honor of being my Valentine?
       </h1> */}
       <div>
-        {Array.from(Array(100).keys()).map((el) => {
+        {Array.from(Array(50).keys()).map((el) => {
           return <FlyingHeart key={el} />;
         })}
       </div>
